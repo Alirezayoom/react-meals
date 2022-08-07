1a9b1bbbad2a46f759558dafa4371902f84df1c8
alireza <alirezayoom@gmail.com>
2022-08-07 12:40:46 +0430

    add and remove added to cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon"
 import CartContext from "../../store/cart-context";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
     const cartCtx = useContext(CartContext) ;
+    const [hasHighlighted, setHasHighlighted] = useState(false)
+
+    const {items} = cartCtx;
+    useEffect(()=>{
+        if(cartCtx.items.length === 0){
+            return;
+        }
+        setHasHighlighted(true)
+        const timer = setTimeout(()=>{
+            setHasHighlighted(false)
+        }, 300)
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items])
+
+    
 
     const numberOfCartItems = cartCtx.items.reduce((curNumber, item)=>{
         return curNumber + item.amount;
     }, 0)
 
+    const classesbtn = `${classes.button} ${hasHighlighted ? classes.bump : ''}`
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={classesbtn} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
