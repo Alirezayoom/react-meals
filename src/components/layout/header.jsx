69a9af5cc72f109d8ef6eab647af3a1b504eabde
alirezayoom <alirezayoom@gmail.com>
2023-03-06 16:01:26 +0430

    header cart button added

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,11 +1,12 @@
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./headerCartButton";
 
 export default function Header() {
   return (
     <>
       <header className="relative bg-orange-800 flex justify-between h-16 items-center px-8 z-10">
         <h1 className="text-3xl font-bold">ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className="absolute top-0 w-full -translate-y-12 -skew-y-3 h-96 overflow-hidden">
         <img src={mealsImage} alt="meals image" className="w-full" />
