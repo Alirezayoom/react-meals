493ce85d29a2add7994646027c7e2cf37a42b683
alirezayoom <alirezayoom@gmail.com>
2023-03-06 15:40:11 +0430

    header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/layout/header";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <div className="bg-red-600">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/layout/header.jsx
@@ -0,0 +1,15 @@
+import mealsImage from "../../assets/meals.jpg";
+
+export default function Header() {
+  return (
+    <>
+      <header className="relative bg-orange-800 flex justify-between h-16 items-center px-8 z-10">
+        <h1 className="text-3xl font-bold">ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className="absolute top-0 w-full -translate-y-12 -skew-y-3 h-96 overflow-hidden">
+        <img src={mealsImage} alt="meals image" className="w-full" />
+      </div>
+    </>
+  );
+}
