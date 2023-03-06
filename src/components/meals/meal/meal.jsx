caa4b102d8f7f4729e0369b1bf47a187eb8c5783
alirezayoom <alirezayoom@gmail.com>
2023-03-06 23:55:30 +0430

    mealForm added to meal

--- src/components/meals/meal/meal.jsx
+++ src/components/meals/meal/meal.jsx
@@ -1,11 +1,18 @@
+import MealForm from "./mealForm";
+
 export default function Meal({ name, description, price }) {
   const thePrice = `$${price.toFixed(2)}`;
 
   return (
-    <li className=" border-neutral-400 border-b-[1px] py-2">
-      <h3 className="text-black font-bold">{name}</h3>
-      <div className="text-neutral-800">{description}</div>
-      <div className="text-xl text-amber-700 font-bold">{thePrice}</div>
+    <li className=" border-neutral-400 border-b-[1px] py-2 flex justify-between">
+      <div>
+        <h3 className="text-black font-bold">{name}</h3>
+        <div className="text-neutral-800">{description}</div>
+        <div className="text-xl text-amber-700 font-bold">{thePrice}</div>
+      </div>
+      <div>
+        <MealForm />
+      </div>
     </li>
   );
 }
