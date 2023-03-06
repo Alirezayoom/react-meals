5f93944a00478a21cc48bc0a6402ffadccc19cc4
alirezayoom <alirezayoom@gmail.com>
2023-03-06 16:29:13 +0430

    meals summary component added

---
+++ src/components/meals/mealsSummary.jsx
@@ -0,0 +1,17 @@
+export default function MealsSummary() {
+  return (
+    <section className="relative bg-neutral-900 rounded-lg p-6 mx-auto w-[40rem] -mt-48 z-10 text-center">
+      <h2 className="text-2xl font-bold mb-4">
+        Delicious Food, Delivered To You
+      </h2>
+      <p>
+        Choose your favorite meal from our broad selection of available meals
+        and enjoy a delicious lunch or dinner at home.
+      </p>
+      <p>
+        All our meals are cooked with high-quality ingredients, just-in-time and
+        of course by experienced chefs!
+      </p>
+    </section>
+  );
+}
